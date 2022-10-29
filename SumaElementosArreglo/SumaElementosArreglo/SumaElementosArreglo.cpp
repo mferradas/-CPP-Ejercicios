5713ce0dceaa9de754963b1388002ac1828f440d
Maximiliano Andrés E. Ferradás <95547687+mferradas@users.noreply.github.com>
2022-10-28 21:43:30 -0300

    Add files via upload

---
+++ SumaElementosArreglo/SumaElementosArreglo/SumaElementosArreglo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int suma = 0;
+	
+	int arreglo[5] = { 12,24,17,19,52 };
+
+	for (int i = 0; i < 5; i++) {
+		suma += arreglo[i];
+	}
+
+	cout << suma;
+
+	return 0;
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
